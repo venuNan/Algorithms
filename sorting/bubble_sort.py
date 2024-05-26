5d415624d0e2a86189f79785dfb53a6f92ba59c9
venuNan <venureddy3808@gmail.com>
2024-05-26 09:06:08 +0530

    add bubble sort function to the bubble_sort.py file

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,19 @@
+def bubble_sort(arr):
+    """
+    Accepts : it takes an array of length n
+
+    purpose : it sorts an array
+
+    return  : a sorted array
+    """
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(n-1):
+            if arr[j] >arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
+
+a = [5,4,3,2,1]
+res = bubble_sort(a)
+print(res)
